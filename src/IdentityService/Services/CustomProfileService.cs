b88c7858fdb1ff600760f92ee6c0999fc7992533
Mohammadkay <mohamd.al.alkayyali@gmail.com>
2024-04-14 12:07:16 +0300

    add Identity

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService;
+
+public class CustomProfileService : IProfileService
+{
+    private readonly UserManager<ApplicationUser> _userManger;
+
+    public CustomProfileService(UserManager<ApplicationUser> userManger)
+    {
+        _userManger=userManger;
+    }
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user=await _userManger.GetUserAsync(context.Subject);
+        var existingClaim=await _userManger.GetClaimsAsync(user);
+        var claims=new List<Claim>
+        {
+            new Claim("username",user.UserName)
+        };
+        context.IssuedClaims.AddRange(claims);
+        context.IssuedClaims.Add(existingClaim.FirstOrDefault(x=>x.Type==JwtClaimTypes.Name));
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net7.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9787fceb6ecb9619549e3b50e7154681bd8a6a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
